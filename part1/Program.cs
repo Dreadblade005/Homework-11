434bc7f1e028d12ebbc2c0ad1269e8cc79e8bba8
dread <dread@DESKTOP-BS1AKPM>
2020-12-19 04:53:08 -0800

    Add project files.

---
+++ part1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace part1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int totalstudents = 0;
+            var data = System.IO.File.ReadAllText("c:StudentInformation.txt");
+            var arrays = new List<float[]>();
+            var lines = data.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                // Create a new List<float> representing all the commaseparated numbers in this line
+                totalstudents++;
+                var lineArray = new List<float>();
+
+                // Slipt line by , and loop through all the numeric valus
+                foreach (var s in line.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    // Add converted numeric value to our lineArray 
+                    lineArray.Add(Convert.ToInt64(s));
+                    //lineArray.Add(Convert.ToSingle(s));
+                }
+                // Add lineArray to main array
+                arrays.Add(lineArray.ToArray());
+
+
+                // Loop repeats until there are noe more lines
+
+                int i = 0;
+                while (i < lineArray.Count)
+                {
+                    result += Convert.ToInt32(lineArray[i++]);
+                }
+
+                
+               
+            }
+            Console.ReadLine();
+
+           data.Close();
+           int totalscore = result;
+           double average = totalscore / totalstudents;
+           StreamWriter pw = new StreamWriter("StudentStats.txt");
+           pw.WriteLine("The average score of the students was: " + average);
+           pw.Close();
+
+
+        }
+    }
+}
---
+++ part1/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace part1
+{
+   public class Student
+    {
+        private string Fname;
+        private string Lname;
+        public int score; 
+
+        public Student(string fname, string lname, int score)
+        {
+            Fname = fname;
+            Lname = lname;
+            this.score = score;
+        }
+        public void SetFName(string fname)
+        {
+            Fname = fname;
+        }
+        public void SetLname(string lname)
+        {
+            Lname = lname;
+        }
+        public void SetFullName(string fname, string lname)
+        {
+            Fname = fname;
+            Lname = lname;
+        }
+        public void setScore(int score)
+        {
+            this.score = score;
+        }
+        public string GetFname()
+        {
+            return Fname;
+        }
+        public string GetLname()
+        {
+            return Lname;
+        }
+        public int GetScore()
+        {
+            return score;
+        }
+        public CompareStudent(object individual)
+        {
+            if (individual == null) return 1;
+
+            Student otherstudent = individual as Student;
+            if (otherstudent != null)
+            {
+                return this.score.CompareTo(otherstudent.score);
+            }
+            else
+            {
+                throw new ArgumentException("Object is not a student");
+            }
+        }
+       
+
+        }
+
+
+    }
+
